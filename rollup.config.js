3bcc85aac8a2fc482b147756acca7d5659620fe0
Dustin Dowell <dustindowell22@gmail.com>
2018-04-03 00:57:26 -0500

    charset removed - no need to escape double quotes

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,7 @@ const styles = fs.readFileSync('dist/objectView.css', 'utf8')
 export default {
   plugins: [
     replace({
-      OBJECT_VIEW_STYLES: `"${styles.replace(/"/g, '\\"').replace('\n', '')}"`
+      OBJECT_VIEW_STYLES: `"${styles.replace('\n', '')}"`
     })
   ]
 }
